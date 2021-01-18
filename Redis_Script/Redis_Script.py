57d7b565fbc800d2ca69079860d736dd248f6f0f
r0759691 <r0759691@student.thomasmore.be>
2021-01-18 18:01:58 +0100

    update

--- Redis_Script/Redis_Script.py
+++ Redis_Script/Redis_Script.py
@@ -2,19 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import threading
-from pymongo import MongoClient
+import redis
 
-client = MongoClient("mongodb://127.0.01:27017")
-Da_db = client["DaDbBits"]
-col_DaDbBits = Da_db["DaDbBits"]
+r = redis.Redis()
+#col_DaDbBits = "DaDbBits"
+#r.sadd(col_DaDbBits, "Most_valuable_hash")
 
 def scraper():
     page = requests.get('https://www.blockchain.com/btc/unconfirmed-transactions')
-    soup = BeautifulSoup(page.content, 'html.parser')
-    Hash_elem = soup.select("a.sc-1r996ns-0.fLwyDF.sc-1tbyx6t-1.kCGMTY.iklhnl-0.eEewhk.d53qjk-0.ctEFcK")
-    Rest_elem = soup.select("span.sc-1ryi78w-0.cILyoi.sc-16b9dsl-1.ZwupP.u3ufsr-0.eQTRKC")
 
-    content = []    
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    Hash_elem = soup.select("a.sc-1r996ns-0.fLwyDF.sc-1tbyx6t-1.kCGMTY.iklhnl-0.eEewhk.d53qjk-0.ctEFcK")
+
+    Rest_elem = soup.select("span.sc-1ryi78w-0.cILyoi.sc-16b9dsl-1.ZwupP.u3ufsr-0.eQTRKC")
+    #for i in range(len(Rest_elem)):
+    #    print(Rest_elem[i].text)
+
+    content = []
     for i in range(len(Hash_elem)):
         line = []
         line.append(Hash_elem[i].text)
@@ -22,11 +27,10 @@ def scraper():
         line.append(float(Rest_elem[i*3+1].text.replace(" BTC", "")))
         line.append(Rest_elem[i*3+2].text)
         content.append(line)
-    
     content.sort(key=lambda x:x[2])
-    myDaDb = {"Hash": content[-1][0], "Time": content[-1][1], "BTC":  content[-1][2], "Dollars":  content[-1][3]}
-    c = col_DaDbBits.insert_one(myDaDb)
-    print(c.inserted_id)
+
+    r.mset({"Hash": content[-1][0], "Time": content[-1][1], "BTC":  content[-1][2], "Dollars":  content[-1][3]})
+    r.get("Hash")
 
 while True:
     scraper()
--- Scraper/Scraper.py
+++ Scraper/Scraper.py
@@ -5,15 +5,10 @@ import threading
 
 def scraper():
     page = requests.get('https://www.blockchain.com/btc/unconfirmed-transactions')
-
     soup = BeautifulSoup(page.content, 'html.parser')
-
     Hash_elem = soup.select("a.sc-1r996ns-0.fLwyDF.sc-1tbyx6t-1.kCGMTY.iklhnl-0.eEewhk.d53qjk-0.ctEFcK")
-
     Rest_elem = soup.select("span.sc-1ryi78w-0.cILyoi.sc-16b9dsl-1.ZwupP.u3ufsr-0.eQTRKC")
-    #for i in range(len(Rest_elem)):
-    #    print(Rest_elem[i].text)
-
+    
     content = []
     for i in range(len(Hash_elem)):
         line = []
@@ -22,13 +17,11 @@ def scraper():
         line.append(float(Rest_elem[i*3+1].text.replace(" BTC", "")))
         line.append(Rest_elem[i*3+2].text)
         content.append(line)
-    content.sort(key=lambda x:x[2])
 
-    #print(content[-1][0])
-    
+    content.sort(key=lambda x:x[2])
     with open("Blockchain_Scraper_Logfile.txt", "a") as f:
         print(content[-1], file=f)
-
+        
 while True:
     scraper()
     time.sleep(60)
