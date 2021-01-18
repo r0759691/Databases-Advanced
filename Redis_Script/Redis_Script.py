e740b18b58b4e29a7a3a72482e98b7a3bc3e7259
r0759691 <r0759691@student.thomasmore.be>
2021-01-18 23:59:04 +0100

    update

--- Redis_Script/Redis_Script.py
+++ Redis_Script/Redis_Script.py
@@ -6,16 +6,11 @@ import redis
 import json
 
 r = redis.Redis()
-#col_DaDbBits = "DaDbBits"
-#r.sadd(col_DaDbBits, "Most_valuable_hash")
 
 def scraper():
     page = requests.get('https://www.blockchain.com/btc/unconfirmed-transactions')
-
     soup = BeautifulSoup(page.content, 'html.parser')
-
     Hash_elem = soup.select("a.sc-1r996ns-0.fLwyDF.sc-1tbyx6t-1.kCGMTY.iklhnl-0.eEewhk.d53qjk-0.ctEFcK")
-
     Rest_elem = soup.select("span.sc-1ryi78w-0.cILyoi.sc-16b9dsl-1.ZwupP.u3ufsr-0.eQTRKC")
 
     content = []
